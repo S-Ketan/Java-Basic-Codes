40b45611faddda962047b1a6eb238c85f46068c7
Ketan Sharma <ketan10923@gmail.com>
2024-10-01 16:37:19 +0200

    Fibonacci sequence Added

---
+++ Fibonacci.java
@@ -0,0 +1,45 @@
+package JavaBasicCodes;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+
+  // 0,1,1,2,3,5,8,13...
+  public static void main(String[] args) {
+
+    // Non-recursive method
+
+    int n1 = 0;
+    int n2 = 1;
+    int n3;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Fibonaaci using non-recursive method : ");
+    int n = sc.nextInt();
+    System.out.println(n1);
+    for (int i = 1; i < n; i++) {
+      System.out.println(n2);
+      n3 = n1 + n2;
+      n1 = n2;
+      n2 = n3;
+    }
+
+    // Recursive method
+
+    System.out.println("Fibonaaci using recursive method : ");
+    int number = sc.nextInt();
+
+    for (int i = 0; i < number; i++) {
+      System.out.println(fSeries(i));
+    }
+
+  }
+
+  static int fSeries(int n) {
+    return switch (n) {
+      case 0 -> 0;
+      case 1 -> 1;
+      default -> fSeries(n - 1) + fSeries(n - 2);
+    };
+
+  }
+}
